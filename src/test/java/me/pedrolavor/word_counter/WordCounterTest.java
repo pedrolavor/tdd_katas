e6de3f93dcaf1e8ded8e7d7b3fb2abfc3e0ebd09
Pedro Lavôr <thiagolavor33@gmail.com>
2020-09-11 20:31:34 -0400

    improvment: improving word counter test

--- src/test/java/me/pedrolavor/word_counter/WordCounterTest.java
+++ src/test/java/me/pedrolavor/word_counter/WordCounterTest.java
@@ -36,18 +36,42 @@ public class WordCounterTest {
     }
 
     @Test
-    public void test() {
+    public void returnCorrectCounter_WhenPassingOneDifferentWord() {
         Map<String, Integer> result1 = WordCounter.count(" ", "boom");
-        Map<String, Integer> result2 = WordCounter.count(" ", "boom beg boom beg bath bath bridge");
+        Map<String, Integer> result2 = WordCounter.count(" ", "boom boom");
+        Map<String, Integer> result3 = WordCounter.count(" ", "boom boom boom");
 
         assertEquals(1, result1.size());
         assertEquals(Integer.valueOf(1), result1.get("boom"));
 
-        assertEquals(4, result2.size());
+        assertEquals(1, result2.size());
         assertEquals(Integer.valueOf(2), result2.get("boom"));
-        assertEquals(Integer.valueOf(2), result2.get("beg"));
+
+        assertEquals(1, result3.size());
+        assertEquals(Integer.valueOf(3), result3.get("boom"));
+    }
+
+    @Test
+    public void returnCorrectCounter_WhenPassingDifferentWords() {
+        Map<String, Integer> result1 = WordCounter.count(",", "boom,bed,bath");
+        Map<String, Integer> result2 = WordCounter.count(",", "boom,bath,boom,boom,bath,bed");
+        Map<String, Integer> result3 = WordCounter.count(",", "boom,bath,bath,bed,boom,bed,boom,bed,big");
+
+        assertEquals(3, result1.size());
+        assertEquals(Integer.valueOf(1), result1.get("boom"));
+        assertEquals(Integer.valueOf(1), result1.get("bed"));
+        assertEquals(Integer.valueOf(1), result1.get("bath"));
+
+        assertEquals(3, result2.size());
+        assertEquals(Integer.valueOf(3), result2.get("boom"));
         assertEquals(Integer.valueOf(2), result2.get("bath"));
-        assertEquals(Integer.valueOf(1), result2.get("bridge"));
+        assertEquals(Integer.valueOf(1), result2.get("bed"));
+
+        assertEquals(4, result3.size());
+        assertEquals(Integer.valueOf(3), result3.get("boom"));
+        assertEquals(Integer.valueOf(2), result3.get("bath"));
+        assertEquals(Integer.valueOf(3), result3.get("bed"));
+        assertEquals(Integer.valueOf(1), result3.get("big"));
     }
 
 }
